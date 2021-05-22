e29a64b9d040810983b7b16dbd6131501e965489
arpitgupta1090 <arpitgupta1090@gmail.com>
2021-05-22 19:23:17 +0530

    heroku push

--- cricpunch/settings.py
+++ cricpunch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i&pwb@*z!n+7u^mq*6i!0_(g%gwov=2dty#!)6aw&$n_k6up1)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'cricpunch.herokuapp.com']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+
 ]
 
 MIDDLEWARE = [
